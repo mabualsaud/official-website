b9292d041147d3f4e3ff54ceb472e70ba54585d8
Mai Abu Al Saud <m.abualsaud@gmail.com>
2026-05-30 00:05:56 +0300

    Delete cpip directory

--- cpip/worker.js
+++
@@ -1,3 +0,0 @@
-import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.60/+esm";
-const handler = new WebWorkerMLCEngineHandler();
-self.onmessage = (msg) => handler.onmessage(msg);
